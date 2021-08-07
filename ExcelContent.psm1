30e4589b41870178e953fb2ddfad189077647ced
yfa33676 <yfa33676@gmail.com>
2021-08-07 12:59:37 +0900

    Get-ExcelContentとGet-ExcelSheetsのパイプライン対応

--- ExcelContent.psm1
+++ ExcelContent.psm1
@@ -1,140 +1,140 @@
 function Get-ExcelContent{
-  param(
-      [Parameter(Position = 0)]
-      [string[]]$Path = ".\*.xls*", # ファイルパス
-      [string]$Pattern = "" , # 検索文字列
-      [string]$BookName = "", # ブック名
-      [string]$SheetName = "", # シート名
-      [string]$Range = "" # セル範囲
-  )
+    param(
+        [Parameter(ValueFromPipeline)]
+        [psobject]$InputObject, # 入力オブジェクト
+        [Parameter(ValueFromPipeline, Position = 0)]
+        [string]$Path, # ファイルパス
+        [string]$Pattern, # 検索文字列
+        [string]$BookName, # ブック名
+        [string]$SheetName, # シート名
+        [string]$Range # セル範囲
+    )
+    begin {
+        # エクセルを起動
+        $excel = New-Object -ComObject Excel.Application
+    }
+    process {
+        # 対象ブックの取得
+        if($Path -ne ""){
+            $BookFiles = Get-Item -Path $Path | ? Name -match $BookName
+        } else {
+            $BookFiles = $InputObject | ? Name -match $BookName
+        }
 
-  ## 結果CSV
-  #$result = New-Item ".\result.csv" -Type File -Force
+        # ブック毎に繰り返し
+        foreach($BookFile in $BookFiles){
+            # ブックを読み取り専用で開く
+            $book = $excel.Workbooks.Open($BookFile, 0, $true)
 
-  # 対象ブックを取得
-  $books = Get-Item -Path $Path
-  $books = $books | ? Name -match $BookName
+            # 対象シートを取得
+            $sheets = $excel.WorkSheets | ? Name -match $SheetName
 
+            # シート毎に繰り返し
+            foreach($sheet in $sheets){
+                # 対象セル範囲を取得
+                if($Range -eq ""){
+                    $SelectedRange = $sheet.UsedRange
+                } else {
+                    $SelectedRange = $sheet.Range("ZZ99,$Range")
+                }
 
-  # エクセルを起動
-  $excel = New-Object -ComObject Excel.Application
+                # 対象セルを取得
+                try{
+                    # 定数
+                    $constants = $SelectedRange.SpecialCells(
+                        [Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeConstants, ## 定数
+                        [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlNumbers + ## 数値
+                        [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlTextValues ## テキスト
+                    )
+                } catch{
+                    $constants = $null
+                }
+                try{
+                    # 数式
+                    $formulas = $SelectedRange.SpecialCells(
+                        [Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeFormulas, ## 数式
+                        [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlNumbers + ## 数値
+                        [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlTextValues + ## テキスト
+                        [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlLogical ## 論理値
+                    )
+                } catch{
+                    $formulas = $null
+                }
+                # 対象セル = 定数 + 数式
+                $cells = $constants + $formulas
 
-  # ヘッダー
-  $line = "ブック名,シート名,セル番地,値"
-  $line | Write-Output
-  #$line | Add-Content $result.FullName
+                # 対象セル毎に繰り返し
+                foreach($cell in $cells){        
+                    # セルのテキストを取得
+                    $text = $cell.Text -Replace "`n","``n"
+                    if($text -ne "" -and $text -match $Pattern){
+                        [PSCustomObject]@{ブック名 = $book.Name; シート名 = $sheet.Name; セル番地 = $cell.Address($false, $false); 値 = $text} | Write-Output
+                    }
+                }
+            }
+            
+            # ブックを閉じる
+            $book.Close(0)
+        }
 
-  # ブック毎に繰り返し
-  for($i = 0; $i -lt $books.Count; $i++){
-      # ブックを読み取り専用で開く
-      $book = $excel.Workbooks.Open($books[$i].FullName, 0, $true)
+        # リソース解放
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($book) | Out-Null
+    }	
+    end {
+        # エクセルを終了
+        $excel.Quit()
 
-      # 対象シートを取得
-      $sheets = $excel.WorkSheets | ? Name -match $SheetName
-
-      # シート毎に繰り返し
-      foreach($sheet in $sheets){
-          # 対象セル範囲を取得
-          if($Range -eq ""){
-              $SelectedRange = $sheet.UsedRange
-          } else {
-              $SelectedRange = $sheet.Range("ZZ99,$Range")
-          }
-
-          # 対象セルを取得
-          try{
-              # 定数
-              $constants = $SelectedRange.SpecialCells(
-                  [Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeConstants, ## 定数
-                  [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlNumbers + ## 数値
-                  [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlTextValues ## テキスト
-              )
-          } catch{
-              $constants = $null
-          }
-          try{
-              # 数式
-              $formulas = $SelectedRange.SpecialCells(
-                  [Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeFormulas, ## 数式
-                  [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlNumbers + ## 数値
-                  [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlTextValues + ## テキスト
-                  [Microsoft.Office.Interop.Excel.XlSpecialCellsValue]::xlLogical ## 論理値
-              )
-          } catch{
-              $formulas = $null
-          }
-          # 対象セル = 定数 + 数式
-          $cells = $constants + $formulas
-
-          # 対象セル毎に繰り返し
-          foreach($cell in $cells){        
-              # セルのテキストを取得
-              $text = $cell.Text -Replace "`n","``n"
-              if($text -ne "" -and $text -match $Pattern){
-                  $line = $book.Name + "," + $sheet.Name + "," + [string]$cell.Address($false, $false) + "," + $text
-                  $line | Write-Output
-                  #$line | Add-Content $result.FullName
-              }
-          }
-      }
-      $book.Close(0)
-  }
-
-  # エクセルを終了
-  $excel.Quit()
-
-  # リソース解放
-  [System.Runtime.InteropServices.Marshal]::ReleaseComObject($excel) | Out-Null
-  [System.Runtime.InteropServices.Marshal]::ReleaseComObject($book) | Out-Null
+        # リソース解放
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($excel) | Out-Null
+    }
 }
 
 function Get-ExcelSheets{
-  param(
-      [Parameter(Position = 0)]
-      [string[]]$Path = ".\*.xls*", # ファイルパス
-      [string]$BookName = "", # ブック名
-      [string]$SheetName = "" # シート名
-  )
+    param(
+        [Parameter(ValueFromPipeline)]
+        [psobject]$InputObject,
+        [Parameter(ValueFromPipeline, Position = 0)]
+        [string]$Path
+    )
+    begin {
+        # エクセルを起動
+        $excel = New-Object -ComObject Excel.Application
+    }
+    process {
+        # 対象ブックの取得
+        if($Path -ne ""){
+            $BookFiles = Get-Item -Path $Path
+        } else {
+            $BookFiles = $InputObject
+        }
 
-  # 結果CSV
-  #$result = New-Item ".\result.csv" -Type File -Force
+        # ブック毎に繰り返し
+        foreach($BookFile in $BookFiles){
+            # ブックを読み取り専用で開く
+            $book = $excel.Workbooks.Open($BookFile, 0, $true)
 
-  # 対象ブックを取得
-  $books = Get-Item -Path $Path
-  $books = $books | ? Name -match $BookName
+            # 対象シートを取得
+            $sheets = $excel.WorkSheets
 
-  # ヘッダー
-  $line = "ブック名,シート名"
-  $line | Write-Output
-  #$line | Add-Content $result.FullName
+            # シート毎に繰り返し
+            foreach($sheet in $sheets){
+                [PSCustomObject]@{ブック名 = $book.Name; シート名 = $sheet.Name} | Write-Output
+            }
+            
+            # ブックを閉じる
+            $book.Close(0)
+        }
 
-  # エクセルを起動
-  $excel = New-Object -ComObject Excel.Application
+        # リソース解放
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($book) | Out-Null
+    }	
+    end {
+        # エクセルを終了
+        $excel.Quit()
 
-  # ブック毎に繰り返し
-  for($i = 0; $i -lt $books.Count; $i++){
-      # ブックを読み取り専用で開く
-      $book = $excel.Workbooks.Open($books[$i].FullName, 0, $true)
-
-      # 対象シートを取得
-      $sheets = $excel.WorkSheets | ? Name -match $SheetName
-
-      # シート毎に繰り返し
-      foreach($sheet in $sheets){
-          $line = $book.Name + "," + $sheet.Name
-          $line | Write-Output
-          #$line | Add-Content $result.FullName
-      }
-      $book.Close(0)
-  }
-
-  # エクセルを終了
-  $excel.Quit()
-
-  # リソース解放
-  [System.Runtime.InteropServices.Marshal]::ReleaseComObject($excel) | Out-Null
-  [System.Runtime.InteropServices.Marshal]::ReleaseComObject($book) | Out-Null
-
+        # リソース解放
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($excel) | Out-Null
+    }
 }
 
 function Set-ExcelContent{
